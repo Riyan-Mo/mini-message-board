d00aa25a41d889c1c13f7f72314dd9445be263a6
Riyan-Mo <Riyan8308151899@gmail.com>
2024-05-15 12:31:06 +0530

    Added form and views for messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const title = "Mini Mesageboard";
+  res.render('index', { title, messages });
 });
+
+router.get('/new', (req, res, next)=>{
+  const title = "New Message"
+  res.render('form', {title})
+})
+
+router.post('/new', (req, res, next)=>{
+  const {name, text} = req.body;
+  messages.push({text, user: name, added: new Date()});
+  res.redirect('/');
+})
 
 module.exports = router;
